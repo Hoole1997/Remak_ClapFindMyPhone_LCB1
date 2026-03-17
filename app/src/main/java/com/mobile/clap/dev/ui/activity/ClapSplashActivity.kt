482c63b41ee06a39e07a7694f80160cc8a59d144
seeeeal <seeeeal@foxmail.com>
2026-03-17 14:54:46 +0800

    增加引导页

---
+++ app/src/main/java/com/mobile/clap/dev/ui/activity/ClapSplashActivity.kt
@@ -0,0 +1,47 @@
+package com.mobile.clap.dev.ui.activity
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.mobile.clap.dev.R
+
+class ClapSplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_clap_splash)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.rootLayout)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        initViews()
+    }
+
+    private fun initViews() {
+        val btnGetStarted = findViewById<TextView>(R.id.btnGetStarted)
+        val tvPrivacyPolicy = findViewById<TextView>(R.id.tvPrivacyPolicy)
+        val tvTermsOfService = findViewById<TextView>(R.id.tvTermsOfService)
+
+        btnGetStarted.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+
+        tvPrivacyPolicy.setOnClickListener {
+            // TODO: Open Privacy Policy
+        }
+
+        tvTermsOfService.setOnClickListener {
+            // TODO: Open Terms of Service
+        }
+    }
+}
